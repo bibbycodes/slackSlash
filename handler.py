037fb3c08e1f48e734e5a13902d86b5a0ab0bbbd
Robert <bibbycodes@gmail.com>
2020-04-03 08:55:37 +0100

    initial commit

---
+++ handler.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+
+def slack_slash(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": "Yasss Baaatch Werrrrk"
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
